5dc491a23bb0bc4423bd458e4639ca03225de71a
Sonualam <sonualam1599@gmail.com>
2024-04-28 22:33:53 +0530

    added user authentication routes

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,25 @@ import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.routes.js";
 import connectMongoDb from "./db/connectMongoDb.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); //to parse req body
+app.use(express.urlencoded({ extended: true })); // to parse form data
+
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Twitter Api");
+});
 
 app.use("/api/auth", authRoutes);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server connected to port : ${process.env.PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server connected to port : ${PORT}`);
   connectMongoDb();
 });
