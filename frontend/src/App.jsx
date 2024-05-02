3c09871d61ced8a25e9f9e858c4584f29f0d90f9
Sonualam <sonualam1599@gmail.com>
2024-05-02 12:16:46 +0530

    frontend starter template cleanup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import LoginPage from "./pages/auth/LoginPage";
+import SignUpPage from "./pages/auth/SignUpPage";
+
+function App() {
+  return (
+    <div className="flex max-w-6xl mx-auto">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
