cceb11aec4411e214ca04f2f42806bba5923e10a
Sonualam <sonualam1599@gmail.com>
2024-05-03 11:39:45 +0530

    feat: added sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import LoginPage from "./pages/auth/LoginPage";
 import SignUpPage from "./pages/auth/SignUpPage";
+import Sidebar from "./components/common/Sidebar";
 
 function App() {
   return (
     <div className="flex max-w-6xl mx-auto">
+      {/* common for all screens */}
+      <Sidebar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
